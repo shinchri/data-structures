2865ba1eff42c7680e234d54c4f0bb8ae9cc8091
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-07 20:38:59 -0500

    chore: Complete problem 2

--- 2_file_recursion.py
+++ 2_file_recursion.py
@@ -2,6 +2,8 @@
 Problem 2: File Recursion
 """
 
+import os
+
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -18,4 +20,24 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    list = []
+    _find_files(suffix, path, list) 
+    return list
+
+def _find_files(suffix, path, list):
+    
+    if os.path.isfile(path):
+        if path.endswith(suffix):
+            list.append(path)
+
+    if os.path.isdir(path):
+        path_list = os.listdir(path)
+        for a_path in path_list:
+            _find_files(suffix, path + "/" + a_path, list)
+
+    return list
+
+result_list = find_files('.c', "./testdir")
+
+for item in result_list:
+    print(item)
