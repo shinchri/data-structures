ed3309d480af95b45ab9378af614823d69e0457e
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-07 19:49:11 -0500

    chore: Create required files

---
+++ 1_LRU_cache.py
@@ -0,0 +1,44 @@
+"""
+Problem 1: Least Recently Used Cache
+"""
+
+from collections import OrderedDict
+
+class LRU_Cache(object):
+
+    def __init__(self, capacity=5):
+        # Initialize class variables
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent. 
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1);
+our_cache.set(2, 2);
+our_cache.set(3, 3);
+our_cache.set(4, 4);
+
+
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+
+our_cache.set(5, 5) 
+our_cache.set(6, 6)
+
+print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
