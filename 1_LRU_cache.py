901c94c80b256c826ecc9c70c0eb342922bb03f3
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-08 16:23:44 -0500

    test: Include 3 tests

--- 1_LRU_cache.py
+++ 1_LRU_cache.py
@@ -23,15 +23,45 @@ class LRU_Cache(object):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
         self.cache[key] = value
         self.cache.move_to_end(key)
+        
         if len(self.cache) > self.capacity:
+            # pop item from the front
             self.cache.popitem(last = False)
+
+
+### TEST CASES ###
+
+# Case 1
 
 our_cache = LRU_Cache(5)
 
-our_cache.set(1, 1);
-our_cache.set(2, 2);
-our_cache.set(3, 3);
-our_cache.set(4, 4);
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+our_cache.set(5, 5)
+our_cache.set(6, 6)
+
+print(our_cache.get(1)) # returns -1
+print(our_cache.get(6)) # returns 6
+
+# Case 2
+our_cache = LRU_Cache(0)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+
+print(our_cache.get(2)) # returns -1 because the cache size is 0 so no values had actually been set
+
+# Case 3
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
 
 
 print(our_cache.get(1))       # returns 1
