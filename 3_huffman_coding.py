d94664729db5f3a87abe8ebc89c34465557969fe
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-09 10:46:47 -0500

    test: Add edge test case for problem 3

--- 3_huffman_coding.py
+++ 3_huffman_coding.py
@@ -261,3 +261,26 @@ if __name__ == "__main__":
     # The content of the encoded data is: 11000000110000010000100010010110000000110000100010010110110000100000100010010110000000110000010000100010010110
     # The size of the decoded data is: 80
     # The content of the encoded data is: a;alskdjf;askdjfaslkdjf;alskdjf
+
+    # Case 4
+    a_great_sentence = "aaaa"
+
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
+    
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+    
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
+
+    # The size of the data is: 53
+    # The content of the data is: aaaa
+    # The size of the encoded data is: 24
+    # The content of the encoded data is: 0000
+    # The size of the decoded data is: 53
+    # The content of the encoded data is: aaaa
